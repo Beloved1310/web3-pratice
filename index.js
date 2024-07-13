7c24dce8bde354d65c5def278f578701112aaa61
Beloved1310 <adeoluwafisayomi@gmail.com>
2024-07-13 02:27:36 +0100

    feat/add curly braces to index file

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-import express from 'express';
-import Web3 from 'web3';
+import express from "express";
+import { Web3 } from "web3";
 
 // Create a new instance of express
 const app = express();
 const port = 3007;
 
 // Connect to an Ethereum node (you can use a provider like Infura or Alchemy)
-const web3 = new Web3('https://mainnet.infura.io/v3/dcf97963d1f64f01b3eacd02b9f70d08');
+const web3 = new Web3(
+  "https://mainnet.infura.io/v3/dcf97963d1f64f01b3eacd02b9f70d08"
+);
 
 // Endpoint to get balance
-app.get('/balance/:address', async (req, res) => {
+app.get("/balance/:address", async (req, res) => {
   const { address } = req.params;
 
   try {
@@ -17,7 +19,7 @@ app.get('/balance/:address', async (req, res) => {
     const balanceWei = await web3.eth.getBalance(address);
 
     // Convert balance from Wei to Ether
-    const balanceEth = web3.utils.fromWei(balanceWei, 'ether');
+    const balanceEth = web3.utils.fromWei(balanceWei, "ether");
 
     res.send({ address, balance: balanceEth });
   } catch (error) {
@@ -29,4 +31,3 @@ app.get('/balance/:address', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
